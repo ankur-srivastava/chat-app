6bf58c99faaf35ec62c2b61f3a718d711e6e0549
ankur-srivastava <ankurlkw@gmail.com>
2018-06-27 06:58:03 +0530

    added socket io

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A chat app built using node.js
 /server : for node.js code
 
 /public : for HTML's
+
+Heroku URL - https://evening-plateau-12166.herokuapp.com/
--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 /*Use the path module to access public directory*/
 const publicPath = path.join(__dirname, '../public');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 app.use(express.static(publicPath));
 
 app.get('/', (req,res)=>{
-  //res.sendFile(publicPath+'/index.html');
+
 });
 
-app.listen(port, ()=>{
+var httpServer = http.createServer(app);
+var io = socketIO(httpServer);
+
+//app.listen(port, ()=>{
+httpServer.listen(port, ()=>{
   console.log('Server is up at port', port);
 });
