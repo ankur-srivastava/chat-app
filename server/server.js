b4f2c71bf8320978e851f374d76f7a26f095c88c
ankur-srivastava <ankurlkw@gmail.com>
2018-06-29 05:42:57 +0530

    Add Connect and Disconnect Event

--- server/server.js
+++ server/server.js
@@ -13,9 +13,25 @@ app.use(express.static(publicPath));
 app.get('/', (req,res)=>{
 
 });
-
+/*Using HTTP Server instead of express server*/
 var httpServer = http.createServer(app);
+
+/*
+  Get the web socket server for client server communication
+  This makes a set of libraries available at http://localhost:3000/socket.io/socket.io.js
+  In index.html we make use of javascript functions available in socket.io/socket.io.js to open connection with server.
+*/
 var io = socketIO(httpServer);
+
+/*When the client hits the server by accessing URL localhost:3000*/
+io.on('connection', (socket)=>{
+  console.log('new user connected');
+
+  //When browser closes
+  socket.on('disconnect', ()=>{
+    console.log('Browser Closed');
+  });
+});
 
 //app.listen(port, ()=>{
 httpServer.listen(port, ()=>{
